85e2b11ec06a05f56a2889c5b2086633a1a2229f
Martin Grotzke <martin.grotzke@inoio.de>
2023-10-03 01:10:40 +0200

    Push versions to next SNAPSHOTs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.0.1"
-project(":outbox-kafka-spring").version = "2.0.1"
-project(":outbox-kafka-spring-reactive").version = "2.0.1"
+project(":commons").version = "2.0.2-SNAPSHOT"
+project(":outbox-kafka-spring").version = "2.0.2-SNAPSHOT"
+project(":outbox-kafka-spring-reactive").version = "2.0.2-SNAPSHOT"
 
 plugins {
     id("java-library")

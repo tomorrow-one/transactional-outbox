46de3472b09d9740a2a531adf64b402b4a4e0eb5
Martin Grotzke <martin.grotzke@inoio.de>
2023-10-26 23:27:31 +0200

    Push commons version
    Due to the change of the protobuf dependency. To push the minor version should be fine, because binary compatibility is provided. The version update is required, so that the reactive / classic modules get a new dependency and don't pull in the old version with the protobuf dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.0.2-SNAPSHOT"
+project(":commons").version = "2.1.0-SNAPSHOT"
 project(":outbox-kafka-spring").version = "3.0.0-SNAPSHOT"
 project(":outbox-kafka-spring-reactive").version = "3.0.0-SNAPSHOT"
 

6ee4d35a6c6bf029f034013646e95d5fde76d35e
Martin Grotzke <martin.grotzke@inoio.de>
2023-11-06 22:17:18 +0100

    Release version 3.0.0 of classic/reactive version
    This releases MR #221, requires users to change the `OutboxService` to
    `ProtobufOutboxService`. The protobuf dependency is now optional, i.e.
    applications (using protobuf) must explicitly depend on it.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.1.0-SNAPSHOT"
-project(":outbox-kafka-spring").version = "3.0.0-SNAPSHOT"
-project(":outbox-kafka-spring-reactive").version = "3.0.0-SNAPSHOT"
+project(":commons").version = "2.1.0"
+project(":outbox-kafka-spring").version = "3.0.0"
+project(":outbox-kafka-spring-reactive").version = "3.0.0"
 
 plugins {
     id("java-library")

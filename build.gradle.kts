05ca6240af77bf75e63c2cde9cd908be4da352c5
Martin Grotzke <martin.grotzke@inoio.de>
2023-11-14 14:05:13 +0100

    Release version 3.0.1
    Mainly with changes from MR #231 "Fix protobuf classpath dependency
    for classpath scanning"

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.1.1-SNAPSHOT"
-project(":outbox-kafka-spring").version = "3.0.1-SNAPSHOT"
-project(":outbox-kafka-spring-reactive").version = "3.0.1-SNAPSHOT"
+project(":commons").version = "2.1.1"
+project(":outbox-kafka-spring").version = "3.0.1"
+project(":outbox-kafka-spring-reactive").version = "3.0.1"
 
 plugins {
     id("java-library")

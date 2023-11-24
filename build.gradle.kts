16b9f126740b13fe1b4b25a845babb84b3793d44
henrik.adamski <henrik.adamski@tomorrow.one>
2023-11-24 14:39:44 +0100

    Release version 3.1.0 of classic/reactive version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.1.2-SNAPSHOT"
-project(":outbox-kafka-spring").version = "3.0.2-SNAPSHOT"
-project(":outbox-kafka-spring-reactive").version = "3.0.2-SNAPSHOT"
+project(":commons").version = "2.2.0"
+project(":outbox-kafka-spring").version = "3.1.0"
+project(":outbox-kafka-spring-reactive").version = "3.1.0"
 
 plugins {
     id("java-library")

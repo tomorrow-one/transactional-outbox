f6a321732bc1e93f6752a7a680bc31033dde380e
Daniel Rehmann <daniel.rehmann@tomorrow.one>
2024-01-04 11:34:19 +0100

    Update build.gradle.kts
    dependency: Upgrade to spring 6.1 could result in breaking behaviour
    
    Co-authored-by: Martin Grotzke <martin.grotzke@inoio.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.2.2"
-project(":outbox-kafka-spring").version = "3.2.1"
-project(":outbox-kafka-spring-reactive").version = "3.1.2"
+project(":commons").version = "2.3.0"
+project(":outbox-kafka-spring").version = "3.3.0"
+project(":outbox-kafka-spring-reactive").version = "3.2.0"
 
 plugins {
     id("java-library")

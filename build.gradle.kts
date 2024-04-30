6d5c721f8cb8c3870c96225dcc5fa72ae6fbed66
henrik.adamski <henrik.adamski@tomorrow.one>
2024-04-30 14:27:15 +0200

    chore(snapshot): Update to next SNAPSHOT version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.3.2"
-project(":outbox-kafka-spring").version = "3.4.0"
-project(":outbox-kafka-spring-reactive").version = "3.3.0"
+project(":commons").version = "2.3.3-SNAPSHOT"
+project(":outbox-kafka-spring").version = "3.4.1-SNAPSHOT"
+project(":outbox-kafka-spring-reactive").version = "3.3.1-SNAPSHOT"
 
 plugins {
     id("java-library")

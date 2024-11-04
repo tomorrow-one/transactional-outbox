b851bb6f34b40bdd7fc8e58e6d6923a18c992e44
Martin Grotzke <martin.grotzke@inoio.de>
2024-11-04 22:07:38 +0100

    Reactive Tests: handle "toxic not found"
    See e.g.
    https://github.com/tomorrow-one/transactional-outbox/actions/runs/11669616978/job/32497886155?pr=162#step:6:4777
    - where `IOException: [404] toxic not found` is thrown. This happens
    with successful and failed tests, i.e. it doesn't seem to be causing the
    error we're hunting.

--- commons/src/testFixtures/java/one/tomorrow/transactionaloutbox/commons/ProxiedContainerSupport.java
+++ commons/src/testFixtures/java/one/tomorrow/transactionaloutbox/commons/ProxiedContainerSupport.java
@@ -17,7 +17,9 @@ package one.tomorrow.transactionaloutbox.commons;
 
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
+import eu.rekawek.toxiproxy.model.Toxic;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
+import lombok.SneakyThrows;
 import org.testcontainers.containers.ToxiproxyContainer;
 
 import java.io.IOException;
@@ -38,6 +40,7 @@ public interface ProxiedContainerSupport {
      * Cuts the connection by setting bandwidth in both directions to zero.
      * @param shouldCutConnection true if the connection should be cut, or false if it should be re-enabled
      */
+    @SneakyThrows
     default void setConnectionCut(boolean shouldCutConnection) {
         synchronized (isCurrentlyCut) {
             if (shouldCutConnection != isCurrentlyCut.get()) {
@@ -47,14 +50,32 @@ public interface ProxiedContainerSupport {
                         getProxy().toxics().bandwidth(CUT_CONNECTION_UPSTREAM, ToxicDirection.UPSTREAM, 0);
                         isCurrentlyCut.set(true);
                     } else {
-                        getProxy().toxics().get(CUT_CONNECTION_DOWNSTREAM).remove();
-                        getProxy().toxics().get(CUT_CONNECTION_UPSTREAM).remove();
+                        removeToxicIfPresent(CUT_CONNECTION_DOWNSTREAM);
+                        removeToxicIfPresent(CUT_CONNECTION_UPSTREAM);
                         isCurrentlyCut.set(false);
                     }
                 } catch (IOException e) {
                     throw new RuntimeException("Could not control proxy", e);
                 }
             }
+        }
+    }
+
+    private void removeToxicIfPresent(String name) throws IOException {
+        Toxic toxic = getToxic(name);
+        if (toxic != null) {
+            toxic.remove();
+        }
+    }
+
+    private Toxic getToxic(String name) {
+        try {
+            return getProxy().toxics().get(name);
+        } catch (IOException e) {
+            if (e.getMessage().contains("[404]")) {
+                return null;
+            }
+            throw new RuntimeException("Could not get toxic", e);
         }
     }
 

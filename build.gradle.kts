9e7b74f081d28a1baf5ea9702eefc6786af00463
Martin Grotzke <martin.grotzke@inoio.de>
2025-05-03 20:59:58 +0200

    Release classic 3.5.0 and reactive 3.4.0
    Major change here is the introduction of tracing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import java.util.*
 
-project(":commons").version = "2.4.0-SNAPSHOT"
-project(":outbox-kafka-spring").version = "3.5.0-SNAPSHOT"
-project(":outbox-kafka-spring-reactive").version = "3.4.0-SNAPSHOT"
+project(":commons").version = "2.4.0"
+project(":outbox-kafka-spring").version = "3.5.0"
+project(":outbox-kafka-spring-reactive").version = "3.4.0"
 
 plugins {
     id("java-library")
